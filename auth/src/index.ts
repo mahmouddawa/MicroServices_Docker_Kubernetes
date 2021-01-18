ce6c49f41e161b0e7c25439bc29d13f4119bd9a6
Mahmoud Dawa <mahmoud.dawa@natuvion.com>
2021-01-18 19:01:43 +0100

    check the workflow update

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import  mongoose  from 'mongoose' ;
 const start = async()=>{
   if(!process.env.JWT_KEY){
     console.log("testing again asd");
+    console.log("testing again asd");
     throw Error('JWT_KEY must be defined');
   }
   if(!process.env.MONGO_URI){
